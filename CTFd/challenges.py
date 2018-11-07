f2d227ae5d618b581af5172a6c1e11a6643d7930
2much4u <christoffwk@gmail.com>
2018-11-07 16:26:11 -0600

    Allow submissions to hidden challenges

--- CTFd/challenges.py
+++ CTFd/challenges.py
@@ -365,8 +365,6 @@ def chal(chalid):
         print("[{0}] {1} submitted {2} with kpm {3}".format(*data))
 
         chal = Challenges.query.filter_by(id=chalid).first_or_404()
-        if chal.hidden:
-            abort(404)
         chal_class = get_chal_class(chal.type)
 
         # Anti-bruteforce / submitting keys too quickly
